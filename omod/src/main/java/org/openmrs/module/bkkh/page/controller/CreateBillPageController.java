50a7cefd067263f90ee1065bb613974233ba5382
Gitahi Ng'ang'a <gitahi86@gmail.com>
2015-11-24 11:48:14 +0300

    Added extension point for create bill feature.

--- omod/src/main/java/org/openmrs/module/bkkh/page/controller/CreateBillPageController.java
+++ omod/src/main/java/org/openmrs/module/bkkh/page/controller/CreateBillPageController.java
@@ -1,22 +1,22 @@
 package org.openmrs.module.bkkh.page.controller;
 
 import org.openmrs.Patient;
-import org.openmrs.api.PatientService;
 import org.openmrs.module.emrapi.patient.PatientDomainWrapper;
 import org.openmrs.ui.framework.annotation.InjectBeans;
-import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by gitahi on 24/11/15.
  */
-public class HelloWorldPageController {
+public class CreateBillPageController {
 
-    public void controller(
-            @SpringBean("patientService") PatientService patientService,
-            PageModel model,
-            @InjectBeans PatientDomainWrapper patientDomainWrapper) {
-        Patient patient = patientService.getPatient(23);
+    public void get
+            (
+                    @RequestParam("patientId") Patient patient,
+                    PageModel model,
+                    @InjectBeans PatientDomainWrapper patientDomainWrapper
+            ) {
         patientDomainWrapper.setPatient(patient);
         model.addAttribute("patient", patientDomainWrapper);
     }
